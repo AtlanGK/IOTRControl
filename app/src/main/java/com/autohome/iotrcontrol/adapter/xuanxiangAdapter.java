1266e64c8f5999a96a26f14f27bbeb27578b8ed5
COOOOOOKIE <cookie.xc1989@gmail.com>
2021-06-22 16:38:23 +0800

    修改item显示及操作

--- app/src/main/java/com/autohome/iotrcontrol/adapter/xuanxiangAdapter.java
+++ app/src/main/java/com/autohome/iotrcontrol/adapter/xuanxiangAdapter.java
@@ -5,10 +5,12 @@ import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.autohome.iotrcontrol.R;
 import com.autohome.iotrcontrol.data.xuanxiangBean;
+import com.autohome.iotrcontrol.data.zhutiBean;
 import com.autohome.iotrcontrol.xuanxiangConfigActivity;
 
 import java.util.ArrayList;
@@ -58,12 +60,27 @@ public class xuanxiangAdapter extends RecyclerView.Adapter<RecyclerView.ViewHold
 
     public class xuanxiangViewHolder extends RecyclerView.ViewHolder{
         public TextView mTV;
+        public TextView mTVConfig;
+        public EditText mTVEdit;
+        public TextView mTVDele;
+        public TextView mTVUp;
+        public TextView mTVDown;
+        public TextView mTVConfirm;
+        public TextView mTVCancel;
 
         public xuanxiangViewHolder(View itemView) {
             super(itemView);
 
-            mTV = (TextView) itemView.findViewById(R.id.item_title_tv);
-            mTV.setOnClickListener(new View.OnClickListener() {
+            mTV =        (TextView) itemView.findViewById(R.id.item_title_tv);
+            mTVConfig =  (TextView) itemView.findViewById(R.id.item_func_config_tv);
+            mTVEdit =    (EditText) itemView.findViewById(R.id.item_edit_tv);
+            mTVUp =      (TextView) itemView.findViewById(R.id.item_func_up_tv);
+            mTVDown =    (TextView) itemView.findViewById(R.id.item_func_down_tv);
+            mTVDele =    (TextView) itemView.findViewById(R.id.item_func_dele_tv);
+            mTVConfirm = (TextView) itemView.findViewById(R.id.item_confirm_tv);
+            mTVCancel =  (TextView) itemView.findViewById(R.id.item_cancel_tv);
+
+            mTVConfig.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     if(mTV.getTag() instanceof xuanxiangBean){
@@ -74,6 +91,56 @@ public class xuanxiangAdapter extends RecyclerView.Adapter<RecyclerView.ViewHold
                         mContext.startActivity(intent);
                     }
 
+                }
+            });
+            mTV.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(mTV.getTag() instanceof zhutiBean){
+                        mTVEdit.setText(mTV.getText());
+                        mTVEdit.setVisibility   (View.VISIBLE);
+                        mTVConfirm.setVisibility(View.VISIBLE);
+                        mTVCancel.setVisibility (View.VISIBLE);
+                        mTV.setVisibility       (View.INVISIBLE);
+                        mTVUp.setVisibility     (View.INVISIBLE);
+                        mTVDown.setVisibility   (View.INVISIBLE);
+                        mTVConfig.setVisibility (View.INVISIBLE);
+                        mTVDele.setVisibility   (View.INVISIBLE);
+                        mTVEdit.setSelectAllOnFocus(true);
+                        mTVEdit.requestFocus();
+                    }
+                }
+            });
+            mTVConfirm.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(mTV.getTag() instanceof zhutiBean){
+                        mTV.setText(mTVEdit.getText());
+                        mTVEdit.setVisibility   (View.INVISIBLE);
+                        mTVConfirm.setVisibility(View.INVISIBLE);
+                        mTVCancel.setVisibility (View.INVISIBLE);
+                        mTV.setVisibility       (View.VISIBLE);
+                        mTVUp.setVisibility     (View.VISIBLE);
+                        mTVDown.setVisibility   (View.VISIBLE);
+                        mTVConfig.setVisibility (View.VISIBLE);
+                        mTVDele.setVisibility   (View.VISIBLE);
+                    }
+                }
+            });
+            mTVCancel.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(mTV.getTag() instanceof zhutiBean){
+                        //mTVEdit.setText(mTV.getText());
+                        mTVEdit.setVisibility   (View.INVISIBLE);
+                        mTVConfirm.setVisibility(View.INVISIBLE);
+                        mTVCancel.setVisibility (View.INVISIBLE);
+                        mTV.setVisibility       (View.VISIBLE);
+                        mTVUp.setVisibility     (View.VISIBLE);
+                        mTVDown.setVisibility   (View.VISIBLE);
+                        mTVConfig.setVisibility (View.VISIBLE);
+                        mTVDele.setVisibility   (View.VISIBLE);
+                    }
                 }
             });
 
--- app/src/main/java/com/autohome/iotrcontrol/adapter/zhutiAdapter.java
+++ app/src/main/java/com/autohome/iotrcontrol/adapter/zhutiAdapter.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.EditText;
 
 import com.autohome.iotrcontrol.R;
 import com.autohome.iotrcontrol.data.zhutiBean;
@@ -59,12 +60,27 @@ public class zhutiAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> 
 
     public class zhutiViewHolder extends RecyclerView.ViewHolder{
         public TextView mTV;
+        public TextView mTVConfig;
+        public EditText mTVEdit;
+        public TextView mTVDele;
+        public TextView mTVUp;
+        public TextView mTVDown;
+        public TextView mTVConfirm;
+        public TextView mTVCancel;
 
         public zhutiViewHolder(View itemView) {
             super(itemView);
 
-            mTV = (TextView) itemView.findViewById(R.id.item_title_tv);
-            mTV.setOnClickListener(new View.OnClickListener() {
+            mTV =        (TextView) itemView.findViewById(R.id.item_title_tv);
+            mTVConfig =  (TextView) itemView.findViewById(R.id.item_func_config_tv);
+            mTVEdit =    (EditText) itemView.findViewById(R.id.item_edit_tv);
+            mTVUp =      (TextView) itemView.findViewById(R.id.item_func_up_tv);
+            mTVDown =    (TextView) itemView.findViewById(R.id.item_func_down_tv);
+            mTVDele =    (TextView) itemView.findViewById(R.id.item_func_dele_tv);
+            mTVConfirm = (TextView) itemView.findViewById(R.id.item_confirm_tv);
+            mTVCancel =  (TextView) itemView.findViewById(R.id.item_cancel_tv);
+
+            mTVConfig.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     if(mTV.getTag() instanceof zhutiBean){
@@ -74,7 +90,56 @@ public class zhutiAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> 
                         intent.setClass(mContext, gongnengActivity.class);
                         mContext.startActivity(intent);
                     }
-
+                }
+            });
+            mTV.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(mTV.getTag() instanceof zhutiBean){
+                        mTVEdit.setText(mTV.getText());
+                        mTVEdit.setVisibility   (View.VISIBLE);
+                        mTVConfirm.setVisibility(View.VISIBLE);
+                        mTVCancel.setVisibility (View.VISIBLE);
+                        mTV.setVisibility       (View.INVISIBLE);
+                        mTVUp.setVisibility     (View.INVISIBLE);
+                        mTVDown.setVisibility   (View.INVISIBLE);
+                        mTVConfig.setVisibility (View.INVISIBLE);
+                        mTVDele.setVisibility   (View.INVISIBLE);
+                        mTVEdit.setSelectAllOnFocus(true);
+                        mTVEdit.requestFocus();
+                    }
+                }
+            });
+            mTVConfirm.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(mTV.getTag() instanceof zhutiBean){
+                        mTV.setText(mTVEdit.getText());
+                        mTVEdit.setVisibility   (View.INVISIBLE);
+                        mTVConfirm.setVisibility(View.INVISIBLE);
+                        mTVCancel.setVisibility (View.INVISIBLE);
+                        mTV.setVisibility       (View.VISIBLE);
+                        mTVUp.setVisibility     (View.VISIBLE);
+                        mTVDown.setVisibility   (View.VISIBLE);
+                        mTVConfig.setVisibility (View.VISIBLE);
+                        mTVDele.setVisibility   (View.VISIBLE);
+                    }
+                }
+            });
+            mTVCancel.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if(mTV.getTag() instanceof zhutiBean){
+                        //mTVEdit.setText(mTV.getText());
+                        mTVEdit.setVisibility   (View.INVISIBLE);
+                        mTVConfirm.setVisibility(View.INVISIBLE);
+                        mTVCancel.setVisibility (View.INVISIBLE);
+                        mTV.setVisibility       (View.VISIBLE);
+                        mTVUp.setVisibility     (View.VISIBLE);
+                        mTVDown.setVisibility   (View.VISIBLE);
+                        mTVConfig.setVisibility (View.VISIBLE);
+                        mTVDele.setVisibility   (View.VISIBLE);
+                    }
                 }
             });
 
